2a4d238d4248685a4421811d319ddde256509dbf
vmirabile <valentin.mirabile@gmail.com>
2015-07-13 12:48:17 -0400

    Added API Request

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 alert("Test js");
+
+<script>
+(function() {
+  var OlapicAPI = "http://rest.photorank.me/customers/215757/media/photorank?";
+  $.getJSON( OlapicAPI, {
+    auth_token: "0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18"
+  })
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        if ( i === 5 ) {
+          return false;
+        }
+      });
+    });
+})();
+</script>
